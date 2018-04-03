70449f875f3dbdc279d163e9f0b92b0f15f010cc
Andres <ballerlikemike23@gmail.com>
2018-04-02 21:55:35 -0400

    Auth Routes

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const authRoutes = require('./routes/auth-routes');
 
 const app = express();
 
+// set up stylesheets
+app.use(express.static(__dirname + "/public"));
+
 // set up view engine
 app.set('view engine', 'ejs');
+
+// set up routes
+app.use('/auth', authRoutes);
 
 // create home route
 app.get('/', (req, res) => {
