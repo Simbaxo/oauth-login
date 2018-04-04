0f2f6632d6cf683d8d1060f7bcd6993529ea0a7c
Andres <ballerlikemike23@gmail.com>
2018-04-03 22:17:25 -0400

    Cookie Session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -11,6 +13,16 @@ app.use(express.static(__dirname + "/public"));
 
 // set up view engine
 app.set('view engine', 'ejs');
+
+// set up cookie session
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [keys.session.cookieKey]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // connect to mongodb
 mongoose.connect(keys.mongodb.dbURI, () => {
